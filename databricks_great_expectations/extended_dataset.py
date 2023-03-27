fe1014f2d6577124700b46171af35ee03ef82de4
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2023-03-27 17:04:36 -0400

    refactor: clean up class and upversion ge

--- databricks_great_expectations/extended_dataset.py
+++ databricks_great_expectations/extended_dataset.py
@@ -1,80 +1,84 @@
 import json
-import os
+from pathlib import Path
 
-from datetime import datetime
 from great_expectations.dataset import SparkDFDataset
+
 
 class ExtendedSparkDFDataset(SparkDFDataset):
     """ExtendedSparkDFDataset extends the GE data SparkDFDataset for Databricks
-    
+
     Attributes:
         dbutils: the Databricks utilitie module
-        path: String value of lexical file path
-        file_path: concat of path and the GE batch id as json
+        save_path: String value of file path. Needs to start with dbfs:/ or /dbfs/
+        **kwargs: additional keyword arguments are passed to the parent class
     """
-    
-    def __init__(self, spark_df, dbutils, path="/dbfs/data-quality", **kwargs):
+
+    def __init__(self, spark_df, dbutils, save_path: str, **kwargs):
         super().__init__(spark_df=spark_df, **kwargs)
         self.dbutils = dbutils
-        self.path = f"{path}/{datetime.today().strftime('%Y/%m/%d/%H/%M')}"
-        self.file_path = f"{self.path}/{self.batch_kwargs['ge_batch_id']}.json"
-        
-    def get_notebook_metadata(self):
+
+        if save_path.startswith("dbfs:/"):
+            self.save_path = Path(save_path.replace("dbfs:/", "/dbfs/"))
+        elif save_path.startswith("/dbfs/"):
+            self.save_path = Path(save_path)
+        else:
+            raise ValueError("save_path must be a dbfs path.")
+
+    def get_notebook_metadata(self) -> dict:
         return json.loads(
-            self.dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
+            self.dbutils.notebook.entry_point.getDbutils()
+            .notebook()
+            .getContext()
+            .toJson()
         )
-    
-    def _create_output_file(self):
-        if not os.path.exists(self.file_path):
-            try:
-                os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
-            except:
-                raise ValueError(
-                    "Failed to create the file path. Please check the user write permissions"
-                )
-                
-    def _write_results_to_json(self, validation_object):
-        self._create_output_file()
+
+    def set_validation_results(self) -> None:
+        self.add_citation(self.get_notebook_metadata())
+        self.results = self.validate()
+
+    def write_results_to_json(self):
+        self.file_path = self.save_path / f"{self.batch_id}.json"
+
+        self.file_path.parent.mkdir(parents=True, exist_ok=True)
+
         with open(self.file_path, "w") as f:
-            f.write(json.dumps(validation_object.to_json_dict()))
+            results_dict = self.results.to_json_dict()
+            json.dump(results_dict, f, indent=2)
         return True
-    
-    def _assert_expectations(self, validation_object):
-        results = validation_object.get("results")
-        
-        failures = [res for res in results if not res.get("success")]
+
+    def _assert_expectations(self):
+        result = self.results.get("results")
+        failures = [res for res in result if not res.get("success")]
+
+        failure_count = len(failures)
+        expectation_count = len(result)
 
         if failures:
-            assert_message = [
-                f"\n\n{len(failures):d} expectations out of {len(results):d} were not met:\n"
+
+            failed_expectations = [
+                f"\t - Failed Expectation: {result.get('expectation_config').get('expectation_type')}\n"
+                f"\t\t - Expectation Config: {result.get('expectation_config').get('kwargs')}\n"
+                f"\t\t - Expectation Result: {result.get('result')}\n"
+                for result in failures
             ]
-            for failed_result in failures:
-                test_type = failed_result.get("expectation_config").get("expectation_type")
-                test_config = failed_result.get("expectation_config").get("kwargs")
-                test_results = failed_result.get("result")
-                
-                assert_message.append(f'\t - Failed Expectation: "{test_type}"')
-                assert_message.append(f'\t\t - Expectation Config: "{test_config}"')
 
-                if test_results:
-                    assert_message.append(f'\t\t - Expectation Result: "{test_results}"')
+            message = (
+                f"\n\n{failure_count:d} expectations out of {expectation_count:d} were not met:\n",
+                f"\n".join(failed_expectations),
+                f"View logs of expectations at {self.file_path}",
+            )
 
-            assert_message.append(f"\nView logs of expectations at {self.file_path}")
-
-            assert False, "\n".join(assert_message)
+            assert False, "\n".join(message)
         else:
             print(
-                f"""All Expectations Passed: {len(failures):d} expectations out of {len(results):d} were not met
+                f"""All Expectations Passed: {failure_count:d} expectations out of {expectation_count:d} were not met
                 
                 - View logs of expectations at {self.file_path}
                 """
             )
             assert True
-    
+
     def validate_and_save(self):
-        self.add_citation(self.get_notebook_metadata())
-        
-        validation_object = self.validate()
-        
-        self._write_results_to_json(validation_object)
-        self._assert_expectations(validation_object)
+        self.set_validation_results()
+        self.write_results_to_json()
+        self._assert_expectations()
